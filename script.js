9c41cd242bf496fda603de85b76f8ce8e2cb9a0f
Eyoba_Smax <eyobsmax@gmail.com>
2025-02-12 18:50:06 +0300

    Dark Mode functionality with preference using localStorage

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const darkToggler = document.getElementById("theme-toggle");
 
-darkToggler.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-  localStorage.setItem("dark", document.body.classList.contains("dark-mode"));
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  if (localStorage.getItem("dark") === "true") {
-    document.body.classList.add("dark-mode");
-  }
-});
